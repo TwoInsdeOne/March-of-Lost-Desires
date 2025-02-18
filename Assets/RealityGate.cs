a3ef1c0f31cd9eb7bfb607047be5bc6f390240ca
David Oliveira <maeminoyume@gmail.com>
2025-02-18 13:06:40 -0300

    reality gate and desire flower realize animation

--- Assets/RealityGate.cs
+++ Assets/RealityGate.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class RealityGate : MonoBehaviour
 {
     public DesireCollector collector;
+    public float attractionSpeed;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "desire flower")
@@ -13,6 +16,10 @@ public class RealityGate : MonoBehaviour
             collector.FindDesire(id);
             collector.RemoveDistantJoint(id);
             collision.GetComponent<DesireFlower>().Realize();
+            Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
+            Vector2 direction = new Vector2(transform.position.x - collision.transform.position.x, transform.position.y - collision.transform.position.y);
+            
+            rb.AddForce(direction.normalized * attractionSpeed, ForceMode2D.Impulse);
         }
     }
 }
